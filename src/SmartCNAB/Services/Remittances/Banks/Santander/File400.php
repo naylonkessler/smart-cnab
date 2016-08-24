f5f0223d397f80dda64fa07fa6747ac7ce9d84e9
Hugo Amaral <hucharal13@gmail.com>
2016-08-24 14:04:16 -0300

    Added remittance and returning classes for Santander

--- src/SmartCNAB/Services/Remittances/Banks/Santander/File400.php
+++ src/SmartCNAB/Services/Remittances/Banks/Santander/File400.php
@@ -15,4 +15,69 @@ class File400 extends Remittance
      * @var string
      */
     protected $schemaFile = '/schemas/400.json';
+
+    /**
+     * Formats a company inscription code.
+     *
+     * @param  mixed  $value
+     * @param  array  $data
+     * @param  array  $meta
+     * @return mixed
+     */
+    protected function formatDetailCompanyinsccode(
+        $value,
+        array $data = [],
+        array $meta = []
+    ) {
+        return strlen($data['companyinscnum']) == 14? 2 : 1;
+    }
+
+    /**
+     * Formats a late interest date.
+     *
+     * @param  mixed  $value
+     * @param  array  $data
+     * @param  array  $meta
+     * @return mixed
+     */
+    protected function formatDetailLateinteresetdate(
+        $value,
+        array $data = [],
+        array $meta = []
+    ) {
+        return $value?: $data['expiration']->add(new \DateInterval('P1D'));
+    }
+
+    /**
+     * Formats a inscription code.
+     *
+     * @param  mixed  $value
+     * @param  array  $data
+     * @param  array  $meta
+     * @return mixed
+     */
+    protected function formatDetailInsccode(
+        $value,
+        array $data = [],
+        array $meta = []
+    ) {
+        return strlen($data['inscnum']) == 14? 2 : 1;
+    }
+
+    /**
+     * Formats a deadline.
+     *
+     * @param  mixed  $value
+     * @param  array  $data
+     * @param  array  $meta
+     * @return mixed
+     */
+    protected function formatDetailDeadline(
+        $value,
+        array $data = [],
+        array $meta = []
+    ) {
+        $value = $value?: 0;
+        return ($data['inst1'] == 6 || $data['inst2'] == 6)? $value : 0;
+    }
 }
